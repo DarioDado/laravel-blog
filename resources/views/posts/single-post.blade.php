24876f944ccee71eea83c0cf5b87ec6c9a4d5891
Dario Stamenkovic <dario.stamenkovic@tnation.eu>
2019-04-24 10:21:44 +0200

    add post headline image in front-end

--- resources/views/posts/single-post.blade.php
+++ resources/views/posts/single-post.blade.php
@@ -1,4 +1,5 @@
 <div class="blog-post">
+    <img class="headline-image" src="{{ asset('img/image.jpg') }}" alt="image">
     <h2 class="blog-post-title">{{$post->title}}</h2>
     <p class="blog-post-meta">{{$post->created_at->diffForHumans()}} by <a href="#">{{$post->user->name}}</a></p>
 
