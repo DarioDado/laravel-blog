10fc31eaaf55cb76e14ebe1c8d715d5d73f4b017
Dario Stamenkovic <dario.stamenkovic@tnation.eu>
2019-04-25 10:25:56 +0200

    add headline image to show post blade

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,6 +3,7 @@
 
 @section('content')
 <div class="blog-post">
+        <img class="headline-image" src="{{ asset('storage/assets/' . $post->asset->id . $post->asset->name) }}" alt="headline image"> 
         <h2 class="blog-post-title">{{$post->title}}</h2>
         <p class="blog-post-meta">{{$post->created_at->diffForHumans()}} by <a href="#">{{$post->user->name}}</a></p>
 
